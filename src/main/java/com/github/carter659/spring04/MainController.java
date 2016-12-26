a4c87641686f92dbb3d940db486b7b8b2b482026
carter659 <24084182@qq.com>
2016-12-27 01:10:43 +0800

    ok

---
+++ src/main/java/com/github/carter659/spring04/MainController.java
@@ -0,0 +1,56 @@
+package com.github.carter659.spring04;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mysql.jdbc.StringUtils;
+
+@Controller
+public class MainController {
+
+	@Autowired
+	private OrderDao orderDao;
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@PostMapping("/save")
+	public @ResponseBody Map<String, Object> save(@RequestBody Order order) {
+		Map<String, Object> result = new HashMap<>();
+		if (StringUtils.isNullOrEmpty(order.id))
+			order.id = orderDao.insert(order);
+		else {
+			orderDao.update(order);
+		}
+		result.put("id", order.id);
+		return result;
+	}
+
+	@PostMapping("/get")
+	public @ResponseBody Object get(String id) {
+		orderDao.findAll();
+		return orderDao.get(id);
+	}
+
+	@PostMapping("/findAll")
+	public @ResponseBody Object findAll() {
+		return orderDao.findAll();
+	}
+
+	@PostMapping("/delete")
+	public @ResponseBody Map<String, Object> delete(String id) {
+		Map<String, Object> result = new HashMap<>();
+		orderDao.delete(id);
+		result.put("id", id);
+		return result;
+	}
+}
---
+++ src/main/java/com/github/carter659/spring04/Order.java
@@ -0,0 +1,23 @@
+package com.github.carter659.spring04;
+
+import java.util.Date;
+
+/**
+ * 博客出处：http://www.cnblogs.com/GoodHelper/
+ * @author 刘冬
+ *
+ */
+public class Order {
+
+	public String id;
+
+	public String no;
+
+	public Date date;
+
+	public int quantity;
+
+	/**
+	 * 省略 get set
+	 */
+}
